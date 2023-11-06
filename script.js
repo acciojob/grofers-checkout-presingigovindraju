c524ebc34eb3422476fd07c95b3934fc68413aea
presingigovindraju <141930032+presingigovindraju@users.noreply.github.com>
2023-11-06 12:49:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,30 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+  // Select all elements with the class "price" to get the prices
+  const priceElements = document.querySelectorAll(".price");
+
+  // Calculate the total price
+  let totalPrice = 0;
+  priceElements.forEach((priceElement) => {
+    const price = parseFloat(priceElement.textContent);
+    if (!isNaN(price)) {
+      totalPrice += price;
+    }
+  });
+
+  // Create a new row for the total price
+  const table = document.querySelector("table");
+  const newRow = table.insertRow(table.rows.length);
+  const cell1 = newRow.insertCell(0);
+  const cell2 = newRow.insertCell(1);
+
+  // Set the cell values
+  cell1.textContent = "Total Price";
+  cell2.textContent = totalPrice;
+
+  // Style the new row
+  newRow.style.fontWeight = "bold";
 };
 
 getSumBtn.addEventListener("click", getSum);
